f60c4dfe694a10a62ba57c8919cf496c8bc4ac87
chowdary1058 <alapatis671@gmail.com>
2026-07-21 12:20:14 +0530

    day 2

--- oops.py
+++ oops.py
@@ -8,7 +8,7 @@ a2=a("kishore",22,"male")
 
 print(a1.age)
 print(a2.name)'''
-class bankaccount:
+'''class bankaccount:
     def __init__(self,account_number,account_holder_name,balance):
         self.account_number=account_number
         self.account_holder_name=account_holder_name
@@ -30,3 +30,14 @@ class bankaccount:
 
     def get_balance(self):
         return self.__balance
+    '''
+class animal:
+    print("animal sound ")
+class dog(animal):
+    def sound(self):
+        print("bark")
+class cat(animal):
+    def sound(self):
+        print("meow")
+    
+    
